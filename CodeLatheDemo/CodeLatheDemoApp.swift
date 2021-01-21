65e7073f2a0350496d7e50bd59605f730f54d3f5
Josh Freed <josh@jpfreed.com>
2021-01-21 11:47:00 -0500

    Initial Commit

---
+++ CodeLatheDemo/CodeLatheDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeLatheDemoApp.swift
+//  CodeLatheDemo
+//
+//  Created by Josh Freed on 1/21/21.
+//
+
+import SwiftUI
+
+@main
+struct CodeLatheDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
